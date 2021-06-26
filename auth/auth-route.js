207d28a79c9e861b7d19df4094f45392dfa39c75
unknown <harshit14k@gmail.com>
2021-06-26 23:01:31 +0530

    second commit

---
+++ auth/auth-route.js
@@ -0,0 +1,147 @@
+require('dotenv').config()
+const User=require('./user-model')
+const bcrypt=require('bcryptjs')
+const jwt=require('jsonwebtoken')
+
+module.exports=(app)=>{
+    
+    //register 
+    app.post('/api/auth/register',(req,res)=>{
+        const {name,email,password}=req.body
+
+        if(!name||!email||!password)
+        {
+            return res.status(400).json({msg:'Please enter all fields'})
+        }
+        User.findOne({email})
+            .then(user=>{
+                if(user)
+                {
+                    return res.status(400).json({msg:'user already exist'})
+                }
+                else
+                {
+                    const newUser=new User({
+                        name,
+                        email,
+                        password
+                    })
+
+                    bcrypt.genSalt(10,(err,salt)=>{
+                        bcrypt.hash(newUser.password,salt,(err,hash)=>{
+                            if(err) throw err;
+
+                            newUser.password=hash;
+                            newUser.save()
+                                    .then(user=>{
+                                        jwt.sign(
+                                            {id:user.id},
+                                            process.env.jwtSecret,
+                                            {expiresIn:7*3600},
+                                            (err,token)=>{
+                                                if(err) throw err;
+
+                                                res.json({
+                                                    token,
+                                                    user:{
+                                                        id:user.id,
+                                                        name:user.name,
+                                                        email:user.email
+                                                    }
+                                                })
+                                            }
+                                        )
+                                    })
+                                    .catch(err=>{
+                                        return res.status(500).json(err)
+                                    })
+                        })
+                    })
+
+                }
+            })
+            .catch(err=>{
+                return res.status(500).json(err)
+            })
+        
+
+
+    })
+
+    //login
+    app.post('/api/auth/login',(req,res)=>{
+        const {email,password}=req.body
+        if(!email||!password)
+        {
+            return res.status(400).json({msg:"please enter all fields"})
+        }
+
+        User.findOne({email})
+            .then(user=>{
+                if(!user)
+                {
+                    return res.status(400).json({msg:"user does not exist"})
+                }
+
+                //compare password
+                bcrypt.compare(password,user.password)
+                    .then(isMatch=>{
+                        if(!isMatch)
+                        {
+                            return  res.status(400).json({msg:"Invalid User"})
+                        }
+
+                        jwt.sign(
+                            {id:user.id},
+                            process.env.jwtSecret,
+                            {expiresIn:7*3600},
+                            (err,token)=>{
+                                if(err) throw err;
+                                res.json({
+                                    token,
+                                    user:{
+                                        id:user.id,
+                                        name:user.name,
+                                        email:user.email
+                                    }
+                                })
+                            }
+                        )
+
+                    })
+                    .catch(err=>{
+                        return res.status(500).json(err)
+                    })
+            })
+            .catch(err=>{
+                return res.status(500).json(err)
+            })
+
+
+    })
+
+    //get user from token
+    app.get('/api/auth/getUser',(req,res)=>{
+        const token=req.header('x-auth-token')
+
+        if(!token)
+        {
+            return res.status(400).json({msg:"No token,Unauthorised user"})
+        }
+
+        try
+        {
+            const decoded=jwt.verify(token,process.env.jwtSecret)
+            User.findById(decoded.id)
+                .select('-password')
+                .then(user=>res.status(200).json(user))
+                .catch(err=>res.json(err))
+
+        }
+        catch(e)
+        {
+            return res.status(400).json({msg:"Token is not valid"})
+        }
+    })
+
+}
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const http=require('http')
 const express=require('express')
-
+const mongoose = require('mongoose')
 const bodyParser=require('body-parser')
 const cors=require('cors')
-
+const  nodemailer = require('nodemailer')
 const app=express()
-
+require('dotenv').config()
 app.use(bodyParser.json())
 app.use(cors())
 
@@ -28,10 +28,10 @@ const io=new Server(server,{
 const Room=require('./room')
 
 const socketToPeerHashMap={} 
-
+let share_id=null;
 io.on('connection',(socket)=>{
     socket.emit('get:peerId')
-    console.log('connected')
+    console.log('connected') 
     console.log(socket.id); 
 
     socket.on('send:peerId',(peerId)=>{
@@ -39,23 +39,56 @@ io.on('connection',(socket)=>{
         socketToPeerHashMap[socket.id]=peerId
     }) 
         
-    socket.on('send-message',(message,roomId)=>{
+    socket.on('send-message',(message,roomId,currentUserId,name)=>{
+         console.log(message)
+       // socket.broadcast.emit('receive-message',message);
+       console.log("roomID",roomId)
+       console.log("backend message",message)
+        socket.broadcast.emit('receive-message',message,roomId,currentUserId,name) 
        
-        socket.broadcast.emit('receive-message',message);
-        console.log(message)
+    }) 
+
+   socket.on('user-screen-share-id',(share_id)=>{
+        console.log("i am sharing my screen and my id is ",share_id);
+        share_id=share_id;
+        socket.broadcast.emit("id-of-person-sharing-screen",share_id);
+        console.log("i am server and send your userid")
+   })
+
+   socket.on("user-stop-sharing",(roomid)=>{
+       socket.broadcast.emit("stop-sharing",roomid)
+       console.log("user-stop-sharing")
+   }) 
+    socket.on('raise-hand',(name,roomId)=>{
+        console.log("i have share hand",name)
+        console.log("with room id",roomId)
+        socket.broadcast.emit('receive-raise-hand',name,roomId) 
     })
-
-
-
-    socket.on('disconnect',()=>{
+    socket.on('hand-down',(name,roomId)=>{
+        console.log("i have down the hand",name)
+        console.log("with room id",roomId)
+        socket.broadcast.emit('receive-hand-down',name,roomId) 
+    })
+    socket.on('disconnect',(roomId)=>{
+        concole.log(roomId)
         console.log('disconnect')
         const peerId=socketToPeerHashMap[socket.id]
-        console.log(peerId)
         io.emit("user:left",peerId)
     })
  
      
 })
+
+mongoose.connect(process.env.MONGO_URL,{useUnifiedTopology:true,useNewUrlParser:true})
+.then(()=>{
+    console.log('database connection establish')
+})
+.catch((e)=>{
+  console.log(e) 
+})
+
+
+require("./auth/auth-route")(app)
 
 
 
@@ -75,9 +108,61 @@ app.get('/rooms/:roomId',(req,res)=>{
 app.post('/rooms/:roomId/join',(req,res)=>{
     const room =rooms.find((existingRoom)=>existingRoom.roomId===req.params.roomId)
     room.addParticipants(req.body.participant);
-    //console.log(req.body.participant)
+    console.log("at the server--->",req.body.participant)
     res.json({...room})
 })
+
+///api to frtch name of person who called
+
+app.post('/name',(req,res)=>{
+    const room =rooms.find((existingRoom)=>existingRoom.roomId===req.body.roomId)
+    console.log("----------------req.body----------------",req.body)
+    for(let i=0;i<room.participants.length;i++)
+    {
+        if(room.participants[i].id===req.body.peerId)
+        {
+            console.log("user found we can call")
+            return res.json(room.participants[i].name)
+        }
+    }
+})
+
+app.post('/api/send',(req,res)=>{
+    let transporter = nodemailer.createTransport({
+     host:'smtp.gmail.com',
+     port:465,
+     secure:true,
+     service:'gmail',
+     auth:{
+         user:'my_email_id',
+         pass:process.env.pass
+     }
+    })
+    var x = req.body.to.split(',');
+    for(var i=0;i<x.length;i++)
+    { 
+        console.log(x[i])
+        var options={
+           to:x[i].trim(),
+           subject:"Link to join teams",
+           html:"<h2>Join Link For The Meeting</h2>"+"<h3 style='font-weight:bold'>"+req.body.url+"</h3>"
+        }
+        transporter.sendMail(options,(error,info)=>{
+           if(error)
+           {
+               return console.log(error)
+           }
+           console.log('Message sent : %s',info.messageId);
+           console.log('Preview URL: %s',nodemailer.getTestMessageUrl(info));
+           console.log('link sent')
+       })
+        
+    }
+    console.log(req.body)
+})
+
+
+
 
 const PORT=process.env.PORT||5000;
 server.listen(PORT,()=>{
